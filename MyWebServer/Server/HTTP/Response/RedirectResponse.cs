3137421ee7da64a5341e24b4c3fec3638d2f2013
MJordanov81 <marian.jordanov@gmail.com>
2017-10-07 00:48:39 +0300

    Redirect response update

--- MyWebServer/Server/HTTP/Response/RedirectResponse.cs
+++ MyWebServer/Server/HTTP/Response/RedirectResponse.cs
@@ -2,11 +2,10 @@
 {
     public class RedirectResponse : HttpResponse
     {
-        public string Data { get; private set; }
 
-        public RedirectResponse(string redirectUrl, string data) : base(redirectUrl)
+        public RedirectResponse(string redirectUrl) : base(redirectUrl)
         {
-            this.Data = data;
+
         }
     }
 }
